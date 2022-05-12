8512a023b55add0f7a46a923dc6eabd0d5f28028
ZacharyWin <2816813070@qq.com>
2022-05-12 17:42:43 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import TestPage from './pages/TestPage'
 import ResultPage from './pages/ResultPage'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<TestPage />} />
-      <Route path='/result' element={<ResultPage />}></Route>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/vote' element={<TestPage />} />
+      <Route path='/result' element={<ResultPage />} />
     </Routes>
   )
 }
