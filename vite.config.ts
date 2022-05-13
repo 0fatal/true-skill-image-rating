9936597a50ca01927c045665849be0bfccdfb219
ZacharyWin <2816813070@qq.com>
2022-05-13 23:00:34 +0800

    perf: improve

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style`
+        }
+      ]
+    })
+  ],
+
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true
+      }
+    }
+  },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
