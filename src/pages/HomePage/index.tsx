8bb593c55d4e7e7a72ffa6aa553f49e5e7e55ab4
ZacharyWin <2816813070@qq.com>
2022-05-14 00:15:10 +0800

    feat: add footer and change title

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -5,15 +5,22 @@ const HomePage = () => {
   const navigate = useNavigate()
   return (
     <div className='flex flex-col items-center pt-[240px]'>
-      <h1 className='text-[40px] md:text-[64px]'>TrueSkill投票</h1>
+      <h1 className='text-[32px] md:text-[64px]'>街道空间品质在线投票</h1>
       <div className='space-x-5 md:space-x-10'>
         <Button shape='round' onClick={() => navigate('/vote')} type='primary' size='large'>
           开始投票
         </Button>
+
         {/* <Button shape='round' onClick={() => navigate('/result')} size='large'>
           投票结果分析
         </Button> */}
       </div>
+      <footer className='flex align-text-top mt-[130px] md:mt-16 flex-col md:flex-row px-4'>
+        <h2 className='text-[18px] md:text-[22px]'>投票规则：</h2>
+        <p className='max-w-[500px] text-[14px] md:text-[16px] leading-8 ml-2 text-gray-600'>
+          我们一共设置了5个与街道空间品质相关的问题，您需要根据每个问题，选择您认为最恰当的场景照片。每个问题需要进行10次投票，共50次，耗时5分钟。
+        </p>
+      </footer>
     </div>
   )
 }
